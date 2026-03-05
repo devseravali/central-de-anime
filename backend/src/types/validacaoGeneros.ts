291f11a1bb258ce25536d7fdfe9e48f9e8d834cb
devseravali <devseravali@gmail.com>
2026-03-05 00:01:47 -0300

    Remove arquivos e tipos não utilizados do backend

--- backend/src/types/validacaoGeneros.ts
+++
@@ -1,9 +0,0 @@
-export interface ValidacaoGenero {
-  id: number;
-  nome: string;
-}
-
-export type ValidacaoGeneroInput = {
-  id?: number | string;
-  nome?: string;
-};
--- backend/src/types/validacaoTags.ts
+++
@@ -1,9 +0,0 @@
-export interface ValidacaoTag {
-  id: number;
-  nome: string;
-}
-
-export type ValidacaoTagInput = {
-  id?: number | string;
-  nome?: string;
-};
