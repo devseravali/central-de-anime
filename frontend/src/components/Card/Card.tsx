14a2f56df09e6d649770a031f19d543c7c3125c7
devseravali <devseravali@gmail.com>
2026-09-09 19:30:47 -0300

    fix: corrige a importação do tipo HTMLAttributes no componente Card

--- frontend/src/components/Card/Card.tsx
+++ frontend/src/components/Card/Card.tsx
@@ -1,5 +1,7 @@
+import type { HTMLAttributes } from 'react';
+
 import { StyledCard } from './StyledCard';
 
-export const Card = (props: React.HTMLAttributes<HTMLDivElement>) => {
+export const Card = (props: HTMLAttributes<HTMLDivElement>) => {
     return <StyledCard {...props} />;
 };
