405a0a22d0f33a0be1d7bb02a8f76be892ae0ea0
devseravali <devseravali@gmail.com>
2026-09-10 20:12:34 -0300

    refactor: remove componentes Avatar, Badge, Button e Card da pasta anterior

--- frontend/src/components/Badge/Badge.tsx
+++
@@ -1,7 +0,0 @@
-import type { HTMLAttributes } from 'react';
-
-import { StyledBadge } from './StyledBadge';
-
-export const Badge = (props: HTMLAttributes<HTMLDivElement>) => {
-    return <StyledBadge {...props} />;
-};
