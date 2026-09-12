e88e16a0f254f109b0baa30b19b6195fec277d6e
devseravali <devseravali@gmail.com>
2026-09-12 20:21:37 -0300

    feat: adiciona interface AnimeData para estruturação de dados de animes

---
+++ frontend/src/types/AnimeData.ts
@@ -0,0 +1,14 @@
+export interface AnimeData {
+  id: number;
+  titulo: string;
+  tipo: string;
+  temporada: number;
+  ano: number;
+  quantidadeEpisodios: number;
+  franquiaId: number;
+  estudioId: number;
+  statusId: number;
+  estacaoId: number;
+  sinopse: string;
+  capaUrl: string;
+}
